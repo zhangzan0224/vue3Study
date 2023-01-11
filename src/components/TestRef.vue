0ce26666b0aae7554c91d17edfc4670994a34889
zhangzan0224 <50470425+zhangzan0224@users.noreply.github.com>
2023-01-11 21:39:42 +0800

    添加依赖包

--- src/components/TestRef.vue
+++ src/components/TestRef.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <button @click="showName">点我</button>
+    <button @click="changeName">修改</button>
   </div>
 </template>
 <script>
@@ -11,9 +12,13 @@ export default {
     const showName = () => {
       alert(`我叫${userName.value}`);
     };
+    const changeName = () => {
+      console.log(userName);
+    };
     return {
       userName,
-      showName
+      showName,
+      changeName,
     };
   },
 };

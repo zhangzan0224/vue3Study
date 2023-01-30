4a4b62b9df0529a57bda9e2eefd049b4e0d381aa
zhangzan0224 <50470425+zhangzan0224@users.noreply.github.com>
2023-01-30 14:46:13 +0800

    计算属性

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,12 @@ const routes = [
     path: "/emitTest",
     component: () => import("@/testEmit/ParentTemp.vue"),
   },
+  // 计算属性测试组件导入
+  {
+    name: "ComputedTest",
+    path: "/computedtest",
+    component: () => import("@/testComputed/index.vue"),
+  },
 ];
 
 export default routes;
---
+++ src/testComputed/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h1>测试计算属性</h1>
+    姓:<input type="text" v-model="person.firstName" />
+    <br />
+    名:<input type="text" v-model="person.lastName" />
+    <br />
+    <span>{{ person.fullName }}</span>
+    <br />
+    全名:<input type="text" v-model="person.fullName" />
+  </div>
+</template>
+<script>
+import { reactive, computed } from "vue";
+export default {
+  name: "ComputedCom",
+  setup() {
+    let person = reactive({
+      firstName: "张",
+      lastName: "三",
+    });
+    // 简写形式,未考虑设置的形式
+
+    /* person.fullName = computed(() => {
+      return person.firstName + "-" + person.lastName;
+    }); */
+    // 复杂形式
+    person.fullName = computed({
+      get() {
+        return person.firstName + "-" + person.lastName;
+      },
+      set(value) {
+        const tempArr = value.split("-");
+        person.firstName = tempArr[0];
+        person.lastName = tempArr[1];
+      },
+    });
+    return { person };
+  },
+};
+</script>
+<style lang="scss" scoped></style>

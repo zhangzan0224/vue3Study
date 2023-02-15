d2af8d289eabff145ad18f6c19cc012e181693fc
Zhangzan <zhangzan0224@126.com>
2023-02-15 23:28:43 +0800

    异步返回数据

--- src/testsetup/SetupDemo.vue
+++ src/testsetup/SetupDemo.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class='container'>
+  <div class="container">
     <span>SetupDemo</span>
+    <h3>{{ sum }}</h3>
   </div>
 </template>
 <script>
 // import { reactive, toRefs, onBeforeMount, onMounted } from "vue";
+
+import { ref } from 'vue'
 export default {
   name: 'SetupDemo',
   setup(props, context) {
     console.log('====================================')
     console.log(props, context)
     console.log('====================================')
+    let sum = ref(0)
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ sum })
+      }, 3000)
+    })
   }
 }
 </script>
-<style lang='scss' scoped></style>
+<style lang="scss" scoped></style>

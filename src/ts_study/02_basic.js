89c8846a538b8ac0d1d14b84a37f8747155adba4
Zhangzan <zhangzan0224@126.com>
2023-03-10 21:20:12 +0800

    添加数组中对象去重

--- src/ts_study/02_basic.js
+++ src/ts_study/02_basic.js
@@ -4,7 +4,63 @@
 // console.log(num)
 // console.log(...arr)
 // @ts-ignore
-function sum(a, b) {
-    return a + b;
+// function sum(a, b) {
+//     return a + b;
+// }
+// console.log(sum(1, 2))
+
+// const abc = [1, 1, 2, 4, 2, 4, 3, 2, 3]
+// const bcd = [...abc]
+
+// for (let i = 0; i < bcd.length; i++) {
+//   for (let j = i + 1; j < bcd.length; j++) {
+//     if (bcd[i] === bcd[j]) {
+//       console.log(j)
+//       bcd.splice(j, 1)
+//       j--
+//       console.log(j)
+//     }
+//   }
+// }
+// 对比两个数组去重
+const arr = [
+  { a: 1, b: 2 },
+  { b: 2, a: 1 },
+  { a: 1, b: 2, c: { a: 1, b: 2 } },
+  { b: 2, a: 1, c: { b: 2, a: 1 } }
+]
+const bcd = [...arr]
+for (let i = 0; i < bcd.length; i++) {
+  for (let j = i + 1; j < bcd.length; j++) {
+    if (equals(bcd[i], bcd[j])) {
+      bcd.splice(j, 1)
+      j--
+    }
+  }
 }
-console.log(sum(1, 2));
+// 对比两个是否相同
+function isObj(val) {
+  return typeof val === 'object' && val !== null
+}
+function equals(val1, val2) {
+  // 两个都是对象的情况下
+  if (isObj(val1) && isObj(val2)) {
+    let key1 = Object.keys(val1)
+    let key2 = Object.keys(val2)
+    if (key1.length !== key2.length) {
+      return false
+    }
+    for (let k of key1) {
+      if (!key2.includes(k)) {
+        return false
+      }
+      if (!equals(val1[k], val2[k])) {
+        return false
+      }
+    }
+    return true
+  } else {
+    return val1 === val2
+  }
+}
+console.log(bcd)

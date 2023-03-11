ca9a2b497f68e3a0866224637e1e210eb9bea2e9
zhangzan0224 <50470425+zhangzan0224@users.noreply.github.com>
2023-03-11 20:41:25 +0800

    数组去重封装成函数

--- src/ts_study/02_basic.js
+++ src/ts_study/02_basic.js
@@ -1,3 +1,11 @@
+/*
+ * @Description:添加了一个对比数组中是否存在需要去除重复的问题
+ * @Version: 2.0
+ * @Author: ZhangZan
+ * @Date: 2023-03-11 20:34:29
+ * @LastEditors: ZhangZan
+ * @LastEditTime: 2023-03-11 20:40:31
+ */
 // 声明一个num为number类型,只能是数字类型
 // let num: number = 123;
 // let arr: number[] = [1, 2, 3]
@@ -29,6 +37,9 @@ const arr = [
   { a: 1, b: 2, c: { a: 1, b: 2 } },
   { b: 2, a: 1, c: { b: 2, a: 1 } }
 ]
+// 去除重复
+// arrayDeDuplication
+function arrayDeDuplication(arr) {
 const bcd = [...arr]
 for (let i = 0; i < bcd.length; i++) {
   for (let j = i + 1; j < bcd.length; j++) {
@@ -38,10 +49,21 @@ for (let i = 0; i < bcd.length; i++) {
     }
   }
 }
+return bcd
+}
+
 // 对比两个是否相同
 function isObj(val) {
   return typeof val === 'object' && val !== null
 }
+
+/**
+ * @description: 判断对象是否重复
+ * @param {*} val1
+ * @param {*} val2
+ * @return {*}
+ * @author: ZhangZan
+ */
 function equals(val1, val2) {
   // 两个都是对象的情况下
   if (isObj(val1) && isObj(val2)) {
@@ -63,4 +85,4 @@ function equals(val1, val2) {
     return val1 === val2
   }
 }
-console.log(bcd)
+console.log(arrayDeDuplication(arr))
